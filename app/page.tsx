c311855f7bed957892064b8695aa4ebb2529fe48
vitor852 <vitor.dc@hotmail.com>
2024-05-01 13:01:28 -0300

    feat: add promo banner to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
 
@@ -8,6 +10,22 @@ export default function Home() {
 
       <div className="px-5 pt-6">
         <Search />
+      </div>
+
+      <div className="px-5 pt-6">
+        <CategoryList />
+      </div>
+
+      <div className="pt-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto em pizzas"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );

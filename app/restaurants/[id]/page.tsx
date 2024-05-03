096289a0620138bfaea20174bb5c4089d458bc1a
vitor852 <vitor.dc@hotmail.com>
2024-05-02 22:45:59 -0300

    feat: add restaurant page

---
+++ app/restaurants/[id]/page.tsx
@@ -0,0 +1,114 @@
+import { notFound } from "next/navigation";
+import Image from "next/image";
+import { StarIcon } from "lucide-react";
+
+import { db } from "@/app/_lib/prisma";
+
+import RestaurantImage from "./_componenents/restaurant-image";
+import DeliveryInfo from "@/app/_components/delivery-info";
+import ProductList from "@/app/_components/product-list";
+
+interface RestaurantPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const RestaurantPage = async ({ params: { id } }: RestaurantPageProps) => {
+  const restaurant = await db.restaurant.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      categories: {
+        include: {
+          products: {
+            where: {
+              restaurantId: id,
+            },
+            include: {
+              restaurant: {
+                select: {
+                  name: true,
+                },
+              },
+            },
+          },
+        },
+      },
+      products: {
+        take: 10,
+        include: {
+          restaurant: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  if (!restaurant) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      <RestaurantImage restaurant={restaurant} />
+
+      <div className="relative mt-[-1.5rem] space-y-4 rounded-tl-3xl rounded-tr-3xl bg-white p-5">
+        <div className="flex justify-between">
+          <div className="flex items-center gap-2">
+            <div className="relative size-10">
+              <Image
+                src={restaurant.imageUrl}
+                alt={restaurant.name}
+                className="rounded-full object-cover"
+                fill
+              />
+            </div>
+
+            <h1 className="truncate text-lg font-bold">{restaurant.name}</h1>
+          </div>
+
+          <div className="flex items-center gap-[4px] rounded-full bg-foreground px-3 py-1 text-white">
+            <StarIcon size={12} className="fill-yellow-500 text-yellow-500" />
+            <span className="text-xs font-bold">5.0</span>
+          </div>
+        </div>
+
+        <DeliveryInfo
+          deliveryFee={Number(restaurant.deliveryFee)}
+          deliveryTimeMinutes={restaurant.deliveryTimeMinutes}
+        />
+
+        <div className="flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          {restaurant.categories.map((category) => (
+            <div
+              className="min-w-[167px] rounded-md bg-gray-100 py-2 text-center text-xs text-muted-foreground"
+              key={category.id}
+            >
+              {category.name}
+            </div>
+          ))}
+        </div>
+
+        <div className="space-y-4">
+          <h2 className="font-semibold">Mais pedidos</h2>
+
+          <ProductList products={restaurant.products} />
+        </div>
+
+        {restaurant.categories.map((category) => (
+          <div className="space-y-4" key={category.id}>
+            <h2 className="font-semibold">{category.name}</h2>
+            <ProductList products={category.products} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantPage;

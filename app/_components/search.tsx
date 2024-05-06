5ed05963ab62502b4bd7766c812becec0c22834f
vitor852 <vitor.dc@hotmail.com>
2024-05-06 00:34:52 -0300

    feat: add search logic

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -1,16 +1,40 @@
+"use client";
+
 import { SearchIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
 const Search = () => {
-  return (
-    <div className="flex gap-2">
-      <Input placeholder="Buscar restaurantes" className="border-none" />
+  const router = useRouter();
 
-      <Button size="icon">
+  function onSearch(e: React.SyntheticEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    const elements = e.currentTarget.elements as typeof form.elements & {
+      search: { value: string };
+    };
+    const searchValue = elements.search.value;
+
+    if (!searchValue) return;
+
+    router.push(`/restaurants?name=${searchValue}`);
+  }
+
+  return (
+    <form className="flex gap-2" onSubmit={onSearch}>
+      <Input
+        placeholder="Buscar restaurantes"
+        className="border-none"
+        name="search"
+      />
+
+      <Button size="icon" type="submit">
         <SearchIcon size={20} />
       </Button>
-    </div>
+    </form>
   );
 };
 

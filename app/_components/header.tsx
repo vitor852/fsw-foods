dd3b418fdb723697924fc804a92c3fefe2b25a3a
vitor852 <vitor.dc@hotmail.com>
2024-05-05 23:06:48 -0300

    feat: header link to home

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -2,11 +2,21 @@ import Image from "next/image";
 
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="flex justify-between px-5 pt-6">
-      <Image src="/logo.png" alt="FSW Foods" height={20} width={100} />
+      <Link href="/">
+        <div className="relative h-[30px] w-[100px]">
+          <Image
+            src="/logo.png"
+            alt="FSW Foods"
+            className="object-cover"
+            fill
+          />
+        </div>
+      </Link>
 
       <Button
         size="icon"

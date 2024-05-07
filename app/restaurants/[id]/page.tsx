9b6a476548fadf89bf07a365adca48463c0afe69
vitor852 <vitor.dc@hotmail.com>
2024-05-07 15:12:48 -0300

    feat: add cart resume component to restaurant page

--- app/restaurants/[id]/page.tsx
+++ app/restaurants/[id]/page.tsx
@@ -7,6 +7,7 @@ import { db } from "@/app/_lib/prisma";
 import RestaurantImage from "./_componenents/restaurant-image";
 import DeliveryInfo from "@/app/_components/delivery-info";
 import ProductList from "@/app/_components/product-list";
+import CartResume from "./_componenents/cart-resume";
 
 interface RestaurantPageProps {
   params: {
@@ -29,6 +30,7 @@ const RestaurantPage = async ({ params: { id } }: RestaurantPageProps) => {
             include: {
               restaurant: {
                 select: {
+                  id: true,
                   name: true,
                 },
               },
@@ -41,6 +43,7 @@ const RestaurantPage = async ({ params: { id } }: RestaurantPageProps) => {
         include: {
           restaurant: {
             select: {
+              id: true,
               name: true,
             },
           },
@@ -57,14 +60,15 @@ const RestaurantPage = async ({ params: { id } }: RestaurantPageProps) => {
     <div>
       <RestaurantImage restaurant={restaurant} />
 
-      <div className="relative mt-[-1.5rem] space-y-4 rounded-tl-3xl rounded-tr-3xl bg-white p-5">
-        <div className="flex justify-between">
+      <div className="relative mt-[-1.5rem] space-y-4 rounded-tl-3xl rounded-tr-3xl bg-white">
+        <div className="flex justify-between px-5 pt-5">
           <div className="flex items-center gap-2">
             <div className="relative size-10">
               <Image
                 src={restaurant.imageUrl}
                 alt={restaurant.name}
                 className="rounded-full object-cover"
+                sizes="100%"
                 fill
               />
             </div>
@@ -78,12 +82,14 @@ const RestaurantPage = async ({ params: { id } }: RestaurantPageProps) => {
           </div>
         </div>
 
-        <DeliveryInfo
-          deliveryFee={Number(restaurant.deliveryFee)}
-          deliveryTimeMinutes={restaurant.deliveryTimeMinutes}
-        />
+        <div className="px-5">
+          <DeliveryInfo
+            deliveryFee={Number(restaurant.deliveryFee)}
+            deliveryTimeMinutes={restaurant.deliveryTimeMinutes}
+          />
+        </div>
 
-        <div className="flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+        <div className="flex gap-3 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
           {restaurant.categories.map((category) => (
             <div
               className="min-w-[167px] rounded-md bg-gray-100 py-2 text-center text-xs text-muted-foreground"
@@ -94,19 +100,23 @@ const RestaurantPage = async ({ params: { id } }: RestaurantPageProps) => {
           ))}
         </div>
 
-        <div className="space-y-4">
+        <div className="space-y-4 pl-5">
           <h2 className="font-semibold">Mais pedidos</h2>
 
           <ProductList products={restaurant.products} />
         </div>
 
-        {restaurant.categories.map((category) => (
-          <div className="space-y-4" key={category.id}>
-            <h2 className="font-semibold">{category.name}</h2>
-            <ProductList products={category.products} />
-          </div>
-        ))}
+        <div className="pl-5">
+          {restaurant.categories.map((category) => (
+            <div className="space-y-4" key={category.id}>
+              <h2 className="font-semibold">{category.name}</h2>
+              <ProductList products={category.products} />
+            </div>
+          ))}
+        </div>
       </div>
+
+      <CartResume />
     </div>
   );
 };

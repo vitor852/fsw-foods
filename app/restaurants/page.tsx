e9004a798794c3ee08ab8c12d3313b6718b9e212
vitor852 <vitor.dc@hotmail.com>
2024-05-07 15:12:31 -0300

    feat: add search component to restaurants page

--- app/restaurants/page.tsx
+++ app/restaurants/page.tsx
@@ -1,11 +1,16 @@
 import Header from "@/app/_components/header";
 
 import Restaurants from "./_components/restaurants";
+import Search from "../_components/search";
 
 const RestaurantsPage = async () => {
   return (
     <>
       <Header />
+
+      <div className="mt-5 px-5">
+        <Search />
+      </div>
 
       <div className="space-y-6 px-5 pt-6">
         <h1 className="text-lg font-semibold">Restaurantes</h1>
